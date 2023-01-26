860bc9006f1636777c3a28545c1b8a47f27ae981
Patrick Dench <denchp@gmail.com>
2023-01-26 14:01:00 -0600

    Added page wrapper to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,12 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Header from '@/components/Header'
+import Page from '@/components/Page'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <Page>
 			<main id="content">
 				<Header />
 				<h1 className="header">Wisconsin Amateur Lacrosse</h1>
@@ -15,6 +16,6 @@ export default function Home() {
 				Lacrosse is an exciting and rewarding sport that provides a unique opportunity for children of all ages to learn, grow, and have fun. It is a fast-paced, physical sport that encourages teamwork, sportsmanship, and respect for the game. With a focus on safety and skill development, lacrosse provides an environment where everyone can thrive and enjoy the sport. From teaching an understanding of the game to building character, lacrosse has something to offer everyone. Join in the fun and become part of the lacrosse family today!
 				</p>
       </main>
-    </>
+    </Page>
   )
 }
