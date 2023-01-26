0c6bfa2f2ab7ec360901f08c65864d00881650f0
Patrick Dench <denchp@gmail.com>
2023-01-26 13:59:05 -0600

    Swiper changes

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -9,6 +9,7 @@ import { DOMElement, useEffect, useRef, useState } from 'react';
 const Carousel = () => {
 	const items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 0"];
 	const swiperRef = useRef<SwiperRef | any>(null);
+	const spacerRef = useRef<HTMLElement>(null);
 	const [ height, setHeight ] = useState(0);
 
 	useEffect(() => {
@@ -20,34 +21,38 @@ const Carousel = () => {
 	}, []);
 
 	return (
-		<div style={{ "minHeight": `${height}px` }}>
-			<Swiper
-				ref={swiperRef}
-				slidesPerView={5}
-				spaceBetween={50}
-				className={styles.swiper}
-				coverflowEffect={{
-					rotate: 20,
-					stretch: 0,
-					depth: -100,
-					modifier: 2,
-				}}
-				effect="coverflow"
-				modules={[EffectCoverflow]}
-			>
-				{
-					items.map(i => 
-					<SwiperSlide 
-						className={styles.item}
-						key={i}
-					>
-						<div className={styles.content}>
-							{i}
-						</div>
-					</SwiperSlide>)
-				}
-			</Swiper>
+		<>
+		<div style={{ "minHeight": `${height}px` }} ref={spacerRef}>
+			
 		</div>
+		<Swiper
+		ref={swiperRef}
+		slidesPerView={5}
+		spaceBetween={50}
+		className={styles.swiper}
+		coverflowEffect={{
+			rotate: 20,
+			stretch: 0,
+			depth: -100,
+			modifier: 2,
+		}}
+		effect="coverflow"
+		modules={[EffectCoverflow]}
+		style={{ top: `${spacerRef.current?.offsetTop}px` }}
+	>
+		{
+			items.map(i => 
+			<SwiperSlide 
+				className={styles.item}
+				key={i}
+			>
+				<div className={styles.content}>
+					{i}
+				</div>
+			</SwiperSlide>)
+		}
+	</Swiper>
+	</>
 	)
 }
 
